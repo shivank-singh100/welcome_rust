d2f94088702528434f5e5c9da6b339f749ea81b2
AdreshSingh <seiyaryuuguuin100@gmail.com>
2024-09-09 05:48:50 +0530

    learnt string creation, loops as well as functions.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    // created a string variable
+    let name="Devank".chars();
+
+    // a loop to iterate in a range similar to kotlin
+    for i in 1..10 {
+        println!("{}",i);
+    } 
+
+    // iterating in a string to print each character
+    for i in name{
+        print!("*{}-",i);
+    }  
+
+    // created a function and invokation
+    add();
+}
+
+// function declaration 
+fn add(){
+    let a=10;
+    let b=20;
+    println!("Sum of two number :{}", (a+b));
 }
